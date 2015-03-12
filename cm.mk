6c5f3bf953370cf604839ebcc8626efa51ad0d9b
Patrik Zakhar <123patokt@gmail.com>
2015-03-12 17:05:14 +0100

    y550: remove non-existing cdma config

--- cm.mk
+++ cm.mk
@@ -14,9 +14,6 @@
 # limitations under the License.
 #
 
-## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/cdma.mk)
-
 # Release name
 PRODUCT_RELEASE_NAME := Y550
 
