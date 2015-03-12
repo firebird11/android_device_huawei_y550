29fbc13ea00a8776571ddf5fcae8137ce7cb1f4b
Patrik Zakhar <123patokt@gmail.com>
2015-03-12 16:27:11 +0100

    y550: remove prebuilt kernel folder

--- Android.mk
+++ Android.mk
@@ -18,18 +18,8 @@
 # This empty Android.mk file exists to prevent the build system from
 # automatically including any other Android.mk files under this directory.
 #
-
-#include $(CLEAR_VARS)
-#LOCAL_MODULE       := wpa_supplicant.conf
-#LOCAL_MODULE_TAGS  := optional
-#LOCAL_MODULE_CLASS := ETC
-#LOCAL_SRC_FILES    := $(LOCAL_MODULE)
-#LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/wifi
-#include $(BUILD_PREBUILT)
-
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),y550)
-include device/huawei/y550/kernel/AndroidKernel.mk
 include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
